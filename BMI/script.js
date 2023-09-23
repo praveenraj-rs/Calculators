bf34b66c34ea49865847fb5e4c3e191717183285
praveenraj-rs <praveenravi515@gmail.com>
2023-09-23 22:38:03 +0530

    added BMI calculator

---
+++ BMI/script.js
@@ -0,0 +1,55 @@
+function compute() {
+  let unit1 = document.querySelector(".unit1").value;
+  let unit2 = document.querySelector(".unit2").value;
+  let wt = parseInt(document.querySelector(".wt").value);
+  let ht = parseInt(document.querySelector(".ht").value);
+  let bmiCal = (bmi(unit1, unit2, wt, ht)).toFixed(1);
+  document.getElementById("result").innerText = bmiCal;
+  
+  
+  if (bmiCal>0 && bmiCal<18.5)
+  {
+    document.getElementById("resultWordP").innerText="Underweight";
+    document.getElementById("resultWord").style.width="150px";
+    document.getElementById("box").style.boxShadow="0px 0px 20px 2px blue";
+    
+  }
+  else if (bmiCal>25 && bmiCal<50)
+  {
+    document.getElementById("resultWordP").innerText="Overweight";
+    document.getElementById("resultWord").style.width="150px";
+    document.getElementById("box").style.boxShadow="0px 0px 20px 2px red";
+  }
+
+}
+
+function bmi(unit1, unit2, wt, ht) {
+  if (unit1 == "lb") {
+    wt = wt / 2.205;
+    // pounds to kg
+  }
+
+  if (unit2 == "cm") {
+    ht = ht / 100;
+    // cm to meters
+  }
+
+  if (unit2 == "f") {
+    ht = ht / 3.281;
+    // feet to meters
+  }
+
+  if (unit2 == "i") {
+    ht = ht * 0.0254;
+    // inches to meters
+  }
+
+  const bm=(wt/ht**2)
+
+  if ((bm<=0 || bm>50)||(wt==0 && ht==0)){
+    alert("Enter Valid Parameters")
+    return 0
+  }
+  
+  else{ return bm}
+}
